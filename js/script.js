856c65e5f26f085bd6bd65cd870aad30c6beae50
Wu-Tang-Name-Here <einarsevilla@gmail.com>
2017-09-06 18:53:54 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,60 +1,153 @@
-var	cats = [
-	{
-		name: "Pablo",
-		imgSrc:"images/blanket.jpg",
-		count: 0
-	},
-	{
-		name: "Kanye",
-		imgSrc:"images/black.jpg",
-		count: 0
-	},
-	{
-		name: "Heidi",
-		imgSrc:"images/box.jpg",
-		count: 0
-	},
-	{
-		name: "Aimee",
-		imgSrc:"images/sad.jpg",
-		count: 0
-	},
-	{
-		name: "Stan",
-		imgSrc:"images/tounge.jpg",
-		count: 0
-	}];
-
-var $catPic= document.getElementById('cat-img')
-var $catName = document.getElementById('catname')
-var $catCount = document.getElementById('cat-count')
-var $catList = document.getElementById('cat-list')
-
-for (var i = 0; i <cats.length; i++) {
-	var cat = cats[i];
-
-	var catList = document.createElement('li');
-	   catList.textContent = cat.name;
-
-	catList.addEventListener('click', (function(catCopy) {
-		return function() {
-			document.getElementById('gif').innerHTML = "";
-			$catPic.src = catCopy.imgSrc;
-            $catName.innerHTML = catCopy.name;
-            $catCount.innerHTML = catCopy.count;
-            currentCat = catCopy;
-		};
-	})(cat));
-
-	$catList.appendChild(catList);
+//////////////MODEL////////////////////////
+var	model = {
+	currentCat: null,
+	cats: [
+		{
+			name: "Pablo",
+			imgSrc:"images/blanket.jpg",
+			count: 0
+		},
+		{
+			name: "Kanye",
+			imgSrc:"images/black.jpg",
+			count: 0
+		},
+		{
+			name: "Heidi",
+			imgSrc:"images/box.jpg",
+			count: 0
+		},
+		{
+			name: "Aimee",
+			imgSrc:"images/sad.jpg",
+			count: 0
+		},
+		{
+			name: "Stan",
+			imgSrc:"images/tounge.jpg",
+			count: 0
+		}
+	],
+	catGif: [
+		{
+			name: "Cat of The Day",
+			imgSrc:"http://thecatapi.com/api/images/get?format=src&type=gif",
+			count: 0
+		}
+	]
 };
 
-//////////Counts number of clicks
+////////////////////OCTOPUS///////////////////////
+var octopus = {
 
-var currentCat
+	init: function() {
+		model.currentCat = model.catGif[0];
 
-$catPic.onclick = function() {
-	currentCat.count += 1;
-	$catCount.innerHTML = currentCat.count;
+		catListView.init();
+		catView.init();
+	},
+
+	getCurrentCat: function() {
+		return model.currentCat;
+	},
+
+	getCats: function(){
+		return model.cats
+	},
+
+	setCurrentCat: function(cat) {
+		model.currentCat = cat; 
+	}, 
+
+	incrementCounter: function() {
+		model.currentCat.count++;
+		catView.render();
+	}
 };
 
+	document.addEventListener ('click', function(){
+		var newProperties = document.getElementById('admin');
+		newProperties = model.currentCat;
+		console.log(newProperties);
+
+		var inputName = document.getElementById('input-name');
+		var inputURL = document.getElementById('input-url');
+		var inputClicks = document.getElementById('input-clicks');
+
+		inputName.value = newProperties.name;
+		inputURL.value = newProperties.imgSrc;
+		inputClicks.value = newProperties.count;
+
+	});
+
+////////////////////////VIEW////////////////////////
+function showForm(){
+	document.getElementById('show-admin').style.visibility="visible";
+	document.getElementById('form-submit').style.visibility="visible";
+}
+
+function hideForm(){
+	document.getElementById('show-admin').style.visibility="hidden";
+}
+
+var catView = {
+
+	init: function() {
+		this.catElem = document.getElementById('cat');
+		this.catPic= document.getElementById('cat-img');
+		this.catName = document.getElementById('catname');
+		this.catCount = document.getElementById('cat-count');
+
+		this.catPic.addEventListener('click', function(){
+			octopus.incrementCounter();
+		});
+		///////save form input/////////
+		document.getElementById('form-submit').addEventListener('click', save);
+		function save(){
+			octopus.render();
+		}
+
+		this.render();
+	},
+
+	render: function() {
+		var currentCat = octopus.getCurrentCat();
+		this.catCount.textContent = currentCat.count;
+		this.catName.textContent = currentCat.name;
+		this.catPic.src = currentCat.imgSrc;
+	}
+}; 
+
+var catListView = {
+		init: function() {
+		this.catListElem = document.getElementById('cat-list');
+
+		this.render();
+	},
+
+	render: function() {
+		var cat, ele, i;
+
+		var cats = octopus.getCats();
+
+		this.catListElem.innerHTML = '';
+
+		for (i = 0; i <cats.length; i++) {
+			cat = cats[i];
+
+			elem = document.createElement('li');
+			elem.textContent = cat.name;
+
+			elem.addEventListener('click', (function(catCopy) {
+				return function () {
+					octopus.setCurrentCat(catCopy);
+					catView.render();
+				};
+			})(cat));
+
+			this.catListElem.appendChild(elem);
+		}
+	}
+};
+
+octopus.init();
